1174b779c486364f9f82747838dfce822db6f29c
David <davidbirinberg@cmail.carleton.ca>
2021-09-17 13:32:14 -0400

    Adding the methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,6 +16,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("tom", "carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
