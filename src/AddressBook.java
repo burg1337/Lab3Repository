30227eab83df3c224ac70aec37dc1a45716497be
David <davidbirinberg@cmail.carleton.ca>
2021-09-17 13:29:50 -0400

    Adding the methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
 public class AddressBook {
+    ArrayList<BuddyInfo> addressBook;
+
+    public AddressBook(){
+        addressBook = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo bi){
+        addressBook.add(bi);
+    }
+
+    public void removeBuddy(BuddyInfo bi){
+        addressBook.remove(bi);
+    }
 
     public static void main(String[] args) {
         System.out.println("Address Book");
