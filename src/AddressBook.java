348fb1642971daac4d8a67f98eac5d1dd22a1b79
David <davidbirinberg@cmail.carleton.ca>
2021-09-24 12:46:17 -0400

    Made a new method for the branch

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -19,6 +19,10 @@ public class AddressBook {
         }
     }
 
+    public void newMethodForBranch(){
+        System.out.println("yo yo new branch");
+    }
+
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("tom", "carleton", "613");
         AddressBook addressBook = new AddressBook();
